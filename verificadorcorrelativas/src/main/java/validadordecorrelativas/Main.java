7afb8d8b60b2b5a0d3e01d30975ff7017f8fe30e
Emanuel Albor <lucas.007.albor.e@gmail.com>
2025-04-08 18:47:50 -0300

    agregado de Ñs

--- verificadorcorrelativas/src/main/java/validadordecorrelativas/Main.java
+++ verificadorcorrelativas/src/main/java/validadordecorrelativas/Main.java
@@ -6,17 +6,17 @@ package validadordecorrelativas;
 		public static void main(String[] args) {
 			Materia algoritmos = new Materia("Algoritmos");
 			Materia paradigmas = new Materia("Paradigmas");
-			Materia diseno = new Materia("Diseno de Sistemas");
+			Materia diseño = new Materia("Diseño de Sistemas");
 	
 			paradigmas.setCorrelativas(new Materia[] { algoritmos });
-			diseno.setCorrelativas(new Materia[] { paradigmas });
+			diseño.setCorrelativas(new Materia[] { paradigmas });
 	
 			Alumno juan = new Alumno("Juan", new Materia[] { algoritmos, paradigmas });
-			Inscripcion insc1 = new Inscripcion(juan, new Materia[] { diseno });
+			Inscripcion insc1 = new Inscripcion(juan, new Materia[] { diseño });
 			System.out.println("Inscripción 1: " + insc1.aprobada()); // true
 	
 			Alumno pedro = new Alumno("Pedro", new Materia[] { algoritmos });
-			Inscripcion insc2 = new Inscripcion(pedro, new Materia[] { diseno });
+			Inscripcion insc2 = new Inscripcion(pedro, new Materia[] { diseño });
 			System.out.println("Inscripción 2: " + insc2.aprobada()); // false
 		}
 	}
